cc5fa0712d47fb8ef9a440a3739237e630e3496f
LosPiesDescalzos <katyaj2001@mail.ru>
2023-06-07 19:47:53 +0300

    add schedule app

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from schedule.views import calendar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schedule/', calendar, name='calendar'),
 ]
