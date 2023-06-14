b83dccefc72a77f6862ee19eedbd72aabb430b2a
LosPiesDescalzos <katyaj2001@mail.ru>
2023-06-15 00:45:27 +0300

    add dinamic change courses

--- schedule/models.py
+++ schedule/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.conf import settings
+from datetime import timedelta
 
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=0, null=True)
+    duration_days = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return self.name
--- schedule/views.py
+++ schedule/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from schedule.models import Review
+from schedule.models import Review, Course
+import random
 
 
 def index(request):
     reviews = Review.objects.filter(rating=5)
-    context = {'reviews': reviews}
+    courses = Course.objects.all()
+    random_courses = random.sample(list(courses), 3)
+
+    context = {
+        'reviews': reviews,
+        'random_courses': random_courses,
+    }
 
     return render(request, 'schedule/index.html', context)
 
