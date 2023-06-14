49aee68314c148e7bdc0342af8e54aee767986a1
LosPiesDescalzos <katyaj2001@mail.ru>
2023-06-14 23:44:51 +0300

    add more data to db

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Account
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'photo']
-
-admin.site.register(Account, ProfileAdmin)
+admin.site.register(Account)
