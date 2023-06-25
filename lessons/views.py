91e040cc50c93050871c4bcea51f542b0149f3d8
jerolongo <styuk1@mail.ru>
2023-06-26 00:07:28 +0300

    fix video embed

--- lessons/views.py
+++ lessons/views.py
@@ -3,35 +3,69 @@ from django.shortcuts import render
 # Create your views here.
 
 def lesson(request, lesson_id):
+    # сейчас есть только лекции 0 и 1, ( /lessons/0 и /lessons/1 )
 
-    questions = [
-        {
-            'number': 1,
-            'body': 'Мы считали дырки в сыре: два плюс три равно ...?',
-            'answer_type': 'radio',
-            'options': ['четыре', 'пять', 'шесть'],
-        },
-        {
-            'number': 2,
-            'body': 'Сколько будет 9 + 10?',
-            'answer_type': 'checkbox',
-            'options': ['12', '13', '19', '4'],
-        },
-        {
-            'number': 3,
-            'body': 'Сколько будет 1 + 10?',
-            'answer_type': 'text',
-        },
-        {
-            'number': 4,
-            'body': 'Сколько будет 9 + 2?',
-            'answer_type': 'text',
-        },
+    questionss = \
+    [
+        [
+            {
+                'number': 1,
+                'body': 'Мы считали дырки в сыре: два плюс три равно ...?',
+                'answer_type': 'radio',
+                'options': ['четыре', 'пять', 'шесть'],
+            },
+            {
+                'number': 2,
+                'body': 'Сколько будет 9 + 10?',
+                'answer_type': 'checkbox',
+                'options': ['12', '13', '19', '4'],
+            },
+            {
+                'number': 3,
+                'body': 'Сколько будет 1 + 10?',
+                'answer_type': 'text',
+            },
+            {
+                'number': 4,
+                'body': 'Сколько будет 9 + 2?',
+                'answer_type': 'text',
+            },
+        ],
+
+        [
+            {
+                'number': 1,
+                'body': 'Как жизнь?',
+                'answer_type': 'text',
+            },
+        ]
     ]
+
+    video_urls = [
+        "https://www.youtube.com/embed/GmESG58YRnY",
+        "https://www.youtube.com/embed/2GgiZZhO-PA",
+    ]
+
+    videos = [
+        {
+            'url': "https://www.youtube.com/embed/GmESG58YRnY",
+            'width': '560px',
+            'height': '315px',
+        },
+        {
+            'url': "https://www.youtube.com/embed/2GgiZZhO-PA",
+            'width': '560px',
+            'height': '315px',
+        },
+
+    ]
+
     context = {
+        'video': videos[lesson_id],
+        'video_url': video_urls[lesson_id],
         'lesson_id': lesson_id,
         'lesson_title': f'Урок {lesson_id}',
-        'questions': questions,
+        'questions': questionss[lesson_id],
     }
 
 
