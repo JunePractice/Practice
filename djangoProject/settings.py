145c53c3d2be82e2d51bebf174c0b18749cbf7c8
deworldgreen123 <deworldgreen123@gmail.com>
2023-06-27 05:25:31 +0300

    fix merge

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'schedule',
     'account',
+    'lessons',
+    'feedback',
+    'notification'
 ]
 
 MIDDLEWARE = [
--- lessons/models.py
+++ lessons/models.py
@@ -2,6 +2,7 @@ from django.db import models
 # from courses.models import Course
 from django.contrib.auth.models import User
 
+
 # удалить, когда будет создана модель courses.models.Course
 class Course(models.Model):
     pass
@@ -21,12 +22,13 @@ class TestQuestion(models.Model):
     answer_type = models.CharField(max_length=255)
     answer_options = models.TextField()
 
+
 class TestResult(models.Model):
     username = models.CharField(max_length=255)
     test = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
 
 class TestAnswer(models.Model):
     result = models.ForeignKey(TestResult, on_delete=models.CASCADE, related_name='answers')
     test_question = models.ForeignKey(TestQuestion, on_delete=models.CASCADE)
     answer = models.TextField()
-
